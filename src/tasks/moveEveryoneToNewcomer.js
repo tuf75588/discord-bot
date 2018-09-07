c2a6ce5048fcd9af66dd05f8f67fd656142f07f2
Andrew Davis <tuf75588@temple.edu>
2018-09-06 22:27:08 -0400

    role automation into discord channel

--- src/tasks/moveEveryoneToNewcomer.js
+++ src/tasks/moveEveryoneToNewcomer.js
@@ -4,11 +4,38 @@ require('dotenv').config();
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}`);
   const guild = client.guilds.get(process.env.GUILD_ID);
-  [...guild.roles.values()].forEach(role => {
-    console.log(role.name);
-  })
+  const allRoles = guild.roles.array();
+  const rolesByName = allRoles.reduce(
+    (byName, role) => ((byName[role.name] = role), byName),
+    {}
+  );
+
+  const otherRoles = allRoles.filter(role => role.name !== '@everyone');
+  const otherRoleMembersById = otherRoles.reduce((memberById, role) => {
+    role.members.array().forEach(({ user }) => {
+      memberById[user.username] = user;
+    });
+    return memberById;
+  }, {});
+
+  const newcomersRole = rolesByName['@newcomers']
+  const everyoneRole = guild.roles.find(role => role.name === '@everyone');
+  Promise.all(
+    everyoneRole.members.map(member => {
+      if (!otherRoleMembersById[member.user.id]) {
+        console.log('Starting to move', member.user.username);
+        return member.addRole(newcomersRole).then(() => {
+          console.log('Moved', member.user.username, 'to @seedling');
+        }).catch(() => {
+          console.log('Error moving', member.user.username, 'to @seedling');
+        });
+      }
+      console.log('Skipping', member.user.username);
+      return Promise.resolve();
+    })
+  ).then(results => {
+    console.log('Checked', results.length, 'users.');
+  });
 });
-
-
 
 client.login(process.env.BOT_TOKEN);
