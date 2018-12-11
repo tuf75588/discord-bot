a448ada860291392ee8e3c9327beb9a47c533ebb
Andrew Davis <atd285@gmail.com>
2018-12-10 21:46:26 -0500

    update package.json

--- src/tasks/moveEveryoneToNewcomer.js
+++
@@ -1,8 +0,0 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
-require('dotenv').config();
-client.on('ready', () => {
-  const guild = client.guilds.get('458369015782440961');
-  console.log(guild);
-});
-client.login('NDg2OTc0ODY0MjcyMzkyMjA0.DvBdIw.IbwukkngIJH3mhzPVz8i_WebeNs');
